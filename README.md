6bcb1b9dd55e10b0bfa2da1ab948a8ebe5800f7a
Andrei Balzan <andreibalzan@gmail.com>
2022-02-28 08:54:13 +0000

    making a simple api with google maps integration

---
+++ README.md
@@ -0,0 +1,3 @@
+based on the course https://www.udemy.com/course/understanding-typescript
+
+Replace the constant MY_API_KEY with your google maps api key.
---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+//To get a key go to `https://developers.google.com/maps/documentation/geocoding/start` and click on get started
+const GOOGLE_API_KEY = 'MY_API_KEY';
+
+type GoogleGeocodeResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    //send this to google's api
+   axios.get<GoogleGeocodeResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+        if(response.data.status !== 'OK'){
+            throw new Error("Could not fetch location")
+        }
+        const coordinates = response.data.results[0].geometry.location;
+
+        //render map
+        const map = new google.maps.Map(document.getElementById('map')!, {
+            center: coordinates,
+            zoom: 8
+        });
+
+        //render marker
+        new google.maps.Marker({
+            position: coordinates,
+            map: map,
+        });
+
+    }).catch(err => {
+        alert(err.message);
+        console.log(err)
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
